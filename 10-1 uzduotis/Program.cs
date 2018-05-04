2dcdb7a9c13fe6b702e2ac115c5d372cd96908a7
Arvydas Banevičius <arvydas.banevicius@gmail.com>
2018-05-04 19:55:58 +0300

    Metodu aprasymo variantai

---
+++ 10-1 uzduotis/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_1_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LabaDiena();
+            AtliktiFunkcija();
+            Console.ReadLine();
+        }
+
+        static void AtliktiFunkcija()
+        {
+            Console.WriteLine("Isvedame teksta");
+        }
+        static void LabaDiena()
+        {
+            Console.WriteLine("Laba diena");
+        }
+    }
+}
---
+++ 10-2 uzduotis/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_2_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prog = new Program();
+            prog.LabaDiena();
+            prog.AtliktiFunkcija();
+            Console.ReadLine();
+        }
+
+        public void AtliktiFunkcija()
+        {
+            Console.WriteLine("Isvedame teksta");
+        }
+        public void LabaDiena()
+        {
+            Console.WriteLine("Laba diena");
+        }
+    }
+}
