d252a338536c6e1ef6e1ceaaf3348b949ec66255
Arvydas Banevičius <arvydas.banevicius@gmail.com>
2018-05-04 20:39:25 +0300

    dar biski

--- 10-4 uzduotis/Program.cs
+++ 10-4 uzduotis/Program.cs
@@ -13,13 +13,37 @@ namespace _10_4_uzduotis
             var program = new Program();
             var vartotojoVardas = program.VartotojoVardas();
             program.Pasisveikinam(vartotojoVardas);
+            var amzius = program.VartotojoAmzius();
+            program.SitoTipoAmzius(amzius);
+
+            var a = program.IvestiSkaiciu(1);
+            var b = program.IvestiSkaiciu(1);
+
+            Console.WriteLine("Gauname rezultata {0}", program.Skaiciujame(a, b));
 
             Console.ReadLine();
         }
 
+        public int IvestiSkaiciu(int Sk)
+        {
+            Console.Write("Iveskite {0} skaiciu: ", Sk);
+            var ats = Convert.ToInt32(Console.ReadLine());
+            return ats;
+        }
+
+        public int Skaiciujame(int sk1, int sk2)
+        {
+            return sk1 + sk2;
+        }
+
         public void Pasisveikinam( string vardas )
         {
-            Console.Write("Sveikas, {0}", vardas);
+            Console.WriteLine("Sveikas, {0}", vardas);
+        }
+
+        public void SitoTipoAmzius(int amzius)
+        {
+            Console.WriteLine("Sito tipo amzius {0}", amzius);
         }
 
         public string VartotojoVardas()
@@ -27,6 +51,13 @@ namespace _10_4_uzduotis
             Console.Write("Iveskite varda: ");
             var vardas = Console.ReadLine();
             return vardas;
+        }
+
+        public int VartotojoAmzius()
+        {
+            Console.Write("Iveskite vartotojo amziu: ");
+            var amzius = Convert.ToInt32( Console.ReadLine() );
+            return amzius;
         }
     }
 }
