fd0fad75e883c94d35fdd23ed9e77f2f342005ae
Arvydas Banevičius <arvydas.banevicius@gmail.com>
2018-05-04 20:26:00 +0300

    Metodai

---
+++ 10-3 uzduotis/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_3_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.LabaDiena();
+            Program.AtliktiFunkcija();
+
+            LabaDiena();
+            AtliktiFunkcija();
+
+            Console.ReadLine();
+        }
+
+        public static void AtliktiFunkcija()
+        {
+            Console.WriteLine("Isvedame teksta");
+        }
+        public static void LabaDiena()
+        {
+            Console.WriteLine("Laba diena");
+        }
+    }
+}
---
+++ 10-4 uzduotis/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_4_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            var vartotojoVardas = program.VartotojoVardas();
+            program.Pasisveikinam(vartotojoVardas);
+
+            Console.ReadLine();
+        }
+
+        public void Pasisveikinam( string vardas )
+        {
+            Console.Write("Sveikas, {0}", vardas);
+        }
+
+        public string VartotojoVardas()
+        {
+            Console.Write("Iveskite varda: ");
+            var vardas = Console.ReadLine();
+            return vardas;
+        }
+    }
+}
