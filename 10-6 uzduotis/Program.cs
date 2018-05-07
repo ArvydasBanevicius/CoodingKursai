7c9540d6d498671fcb6c37ae3ddab19ac9b40d62
Arvydas Banevičius <arvydas.banevicius@gmail.com>
2018-05-07 21:33:10 +0300

    Jau klases

---
+++ 10-6 uzduotis/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_6_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            var Sarasas = new Dictionary<string, Int32>();
+            Sarasas = Ivedimas();
+
+            Console.ReadLine();
+        }
+
+        static Dictionary<string,Int32> Ivedimas()
+        {
+            var sar = new Dictionary<string, Int32>();
+            Console.Write("Iveskite saraso ilgi:");
+            var Ilgis = Convert.ToInt32( Console.Read() );
+            var i = 0;
+            do
+            {
+                try
+                {
+                    var Pavadinimas = Console.ReadLine();
+                    var Reiksme = Convert.ToInt32( Console.ReadLine() );
+                    sar.Add(Pavadinimas, Reiksme);
+                    i++;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Klaidingai ivesti duomenys");
+                    //throw;
+                }
+            }
+            while (i < Ilgis);
+            return sar;
+        }
+    }
+}
---
+++ 11-1 uzduotis/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_1_uzduotis
+{
+    class Automobilis
+    {
+        public int GimimoMetai;
+        public string Spalva;
+        public string Marke;
+        public string VIN;
+        public string VatstyvinisNumeris;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Automobilis auto = new Automobilis();
+            auto.GimimoMetai = 2005;
+            auto.Spalva = "Raudona";
+            Console.WriteLine(auto.GimimoMetai);
+            Console.WriteLine(auto.Spalva);
+
+            Console.ReadLine();
+        }
+    }
+}
