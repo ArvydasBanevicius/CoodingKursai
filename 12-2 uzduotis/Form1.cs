b653eb57726eed6b5e20517a37641dcffb96cbf5
Arvydas Banevičius <arvydas.banevicius@gmail.com>
2018-05-14 21:25:33 +0300

    Naijas

---
+++ 12-2 uzduotis/Form1.cs
@@ -0,0 +1,58 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+
+namespace _12_2_uzduotis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        // https://www.amazon.co.uk/gp/search/ref=sr_il_ti_computers?fst=as%3Aon&rh=k%3Alaptop%2Cn%3A340831031%2Cn%3A429886031&keywords=laptop&ie=UTF8&qid=1526316124&lo=computers
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var url = textBox1.Text;
+            textBox1.Text = "";
+            GautiHTML(url);
+        }
+
+        private static async void GautiHTML(string url)
+        {
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+            var htmlDocoment = new HtmlAgilityPack.HtmlDocument();
+            htmlDocoment.LoadHtml(html);
+            var produktuSarasas = htmlDocoment.DocumentNode.Descendants("ul")
+                .Where(x => x.GetAttributeValue("id", "")
+                .Equals("s-results-list-atf")).ToList();
+
+            var produktuElementai = produktuSarasas[0].Descendants("li")
+                .Where(x => x.GetAttributeValue("id", "")
+                .Contains("result")).ToList();
+
+
+            for (int i=0; i<produktuElementai.Count; i++)
+            {
+            }
+
+            foreach (var htmlNode in produktuElementai)
+            {
+                MessageBox.Show(htmlNode.InnerText);
+            }
+        }
+    }
+}
---
+++ 12-3 uzduotis/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _12_3_uzduotis
+{
+    public partial class Form1 : Form
+    {
+
+        int[,] ZaidziantysSkaiciai = new int[5, 5];
+        public int M, J, R, G, Z;
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                dataGridView1.Rows.Add("", "", "", "", "");
+            }
+        }
+
+
+        private void NaujasZaidimas()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                M = rnd.Next(1, 15);
+                J = rnd.Next(16, 30);
+                R = rnd.Next(31, 45);
+                G = rnd.Next(46, 60);
+                Z = rnd.Next(61, 75);
+//                dataGridView1.CellRows.Add(M, J, R, G, Z);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+        //    e.ColumnIndex
+        }
+    }
+}
